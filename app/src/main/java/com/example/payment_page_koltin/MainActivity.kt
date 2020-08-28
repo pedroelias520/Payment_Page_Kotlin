bc86e0ff0cd2c36561b6ac7c18d12fe3930f880d
Pedro Elias <epedro520@gmail.com>
2020-08-28 12:03:36 -0300

    upgrade:Spinner created

--- app/src/main/java/com/example/payment_page_koltin/MainActivity.kt
+++ app/src/main/java/com/example/payment_page_koltin/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.payment_page_koltin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
+    private var spinner:Spinner ? = null
+    private var arrayAdapter:ArrayAdapter<String> ? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
