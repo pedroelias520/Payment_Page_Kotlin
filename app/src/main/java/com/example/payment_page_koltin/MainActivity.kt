16ba6e2e3c18ed41f4e1730bd7101884781a4606
Pedro Elias <epedro520@gmail.com>
2020-08-29 00:58:39 -0300

    upgrade: page for selecting a item

--- app/src/main/java/com/example/payment_page_koltin/MainActivity.kt
+++ app/src/main/java/com/example/payment_page_koltin/MainActivity.kt
@@ -9,22 +9,45 @@ import kotlinx.android.synthetic.main.payment_page.*
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
 
+    private fun hideSystemUI() {
+        // Ativa o modo imersivo de tela
+        // Para o modo "recuar", remova SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Esconde barra de status e barra de navegação
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+
+                )
+    }
 
     private var spinner:Spinner ? = null
     private var text_view:TextView ? = null
-    private var arrayAdapter:ArrayAdapter<Int> ? = null
+    private var arrayAdapter:ArrayAdapter<String> ? = null
 
-    private var itemList = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12)
+    private var itemList = arrayOf("1","2","3","4","5","6","7","8","9","10","11","12")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.payment_page)
+
 
         spinner = list_parcelas
         text_view = parcelas_text
         arrayAdapter = ArrayAdapter(applicationContext,android.R.layout.simple_spinner_item,itemList)
         spinner?.adapter = arrayAdapter
         spinner?.onItemSelectedListener = this
+
+        editText4.setOnClickListener {
+            nome_produto.text = editText4.text
+            if (nome_produto.text != null) {
+                editText4.setTextColor(resources.getColor(R.color.abc_search_url_text))
+            }
+        }
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
