74e14ce06bcd3a3e4a0f1da64a473afbac461ba0
Pedro Elias <epedro520@gmail.com>
2020-08-28 21:59:54 -0300

    upgrade: Credit Card added

--- app/src/main/java/com/example/payment_page_koltin/MainActivity.kt
+++ app/src/main/java/com/example/payment_page_koltin/MainActivity.kt
@@ -2,18 +2,37 @@ package com.example.payment_page_koltin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.payment_page.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+
 
     private var spinner:Spinner ? = null
-    private var arrayAdapter:ArrayAdapter<String> ? = null
+    private var text_view:TextView ? = null
+    private var arrayAdapter:ArrayAdapter<Int> ? = null
 
+    private var itemList = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.payment_page)
+
+        spinner = list_parcelas
+        text_view = parcelas_text
+        arrayAdapter = ArrayAdapter(applicationContext,android.R.layout.simple_spinner_item,itemList)
+        spinner?.adapter = arrayAdapter
+        spinner?.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        Toast.makeText(applicationContext, "Nada selecionado", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        var items:String = p0?.getItemAtPosition(p2) as String
+        text_view?.text = "${items} x"
     }
 }
